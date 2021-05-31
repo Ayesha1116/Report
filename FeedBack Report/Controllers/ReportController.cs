7b38c194faf9f09a12a7cc0aee092bceed5503d1
Ayesha <ayesha.siddika2@g.bracu.ac.bd>
2021-05-31 09:44:34 +0600

    FeedBack Report

---
+++ FeedBack Report/Controllers/ReportController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using AspNetCore.Reporting;
+
+namespace FeedBack_Report.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ReportController(IWebHostEnvironment webHostEnvironment)
+        {
+            this._webHostEnvironment = webHostEnvironment;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Print()
+        {
+            string mimtype = "";
+            int extension = 1;
+            var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\Report1.rdlc";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("rp1", "Welcome to Report");
+            LocalReport localReport = new LocalReport(path);
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimtype);
+            return File(result.MainStream, "application/pdf ");
+
+        }
+
+    }
+}
